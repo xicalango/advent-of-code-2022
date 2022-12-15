b6bdd5f045f1e8b9a783ce831bf60308a7409c4c
Alexander Weld <weldale@gmail.com>
2022-12-15 21:47:01 +0100

    parse and visualize input

--- src/day14.rs
+++ src/day14.rs
@@ -1,10 +1,11 @@
 use std::cmp::{max, min};
+use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 use crate::Error;
 
 type Position = u32;
 
-#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Default, Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Vec2(Position, Position);
 
 impl FromStr for Vec2 {
@@ -16,34 +17,55 @@ impl FromStr for Vec2 {
     }
 }
 
-impl Vec2 {
 
+
+impl Vec2 {
     pub fn product(&self) -> Position {
         let Vec2(x, y) = self;
         x * y
     }
+}
 
+impl From<(Position, Position)> for Vec2 {
+    fn from(tuple: (Position, Position)) -> Self {
+        let (x,y) = tuple;
+        Vec2(x, y)
+    }
+}
+
+impl From<Vec2> for (Position, Position) {
+    fn from(vec: Vec2) -> Self {
+        let Vec2(x, y) = vec;
+        (x, y)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Line(Vec2, Vec2);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub struct Box(Vec2, Vec2);
 
 impl Box {
 
+    pub fn new() -> Box {
+        Box::default()
+    }
+
+    pub fn new_with_dimension(top_left: Vec2, bottom_right: Vec2) -> Box {
+        Box(top_left, bottom_right)
+    }
+
+    pub fn new_with_size(top_left: Vec2, size: Vec2) -> Box {
+        let Vec2(x, y) = top_left;
+        let Vec2(sx, sy) = size;
+
+        Box(Vec2(x, y), Vec2(x + sx, y + sy))
+    }
+
     pub fn size(&self) -> Vec2 {
         let Box(Vec2(sx, sy), Vec2(ex, ey)) = self;
         Vec2(ex - sx, ey - sy)
-    }
-
-    pub fn top_left(&self) -> &Vec2 {
-        &self.0
-    }
-
-    pub fn bottom_right(&self) -> &Vec2 {
-        &self.1
     }
 
 }
@@ -76,6 +98,22 @@ impl FromStr for LineRow {
 
 pub trait BoundingBox {
     fn get_bounds(&self) -> Box;
+
+    fn get_top_left(&self) -> Vec2 {
+        self.get_bounds().get_top_left()
+    }
+
+    fn get_top_right(&self) -> Vec2 {
+        self.get_bounds().get_top_right()
+    }
+
+    fn get_bottom_left(&self) -> Vec2 {
+        self.get_bounds().get_bottom_left()
+    }
+
+    fn get_bottom_right(&self) -> Vec2 {
+        self.get_bounds().get_bottom_right()
+    }
 }
 
 impl BoundingBox for Vec2 {
@@ -100,7 +138,28 @@ impl BoundingBox for Line {
 impl BoundingBox for Box {
     fn get_bounds(&self) -> Box {
         self.clone()
-    } 
+    }
+
+    fn get_top_left(&self) -> Vec2 {
+        self.0.clone()
+    }
+
+    fn get_top_right(&self) -> Vec2 {
+        let Box(Vec2(_, sy), Vec2(ex, _)) = self;
+
+        Vec2(*ex, *sy)
+    }
+
+    fn get_bottom_left(&self) -> Vec2 {
+        let Box(Vec2(sx, _), Vec2(_, ey)) = self;
+
+        Vec2(*sx, *ey)
+    }
+
+    fn get_bottom_right(&self) -> Vec2 {
+        self.1.clone()
+    }
+
 }
 
 impl<'a, B: 'a + BoundingBox> FromIterator<&'a B> for Box {
@@ -143,45 +202,56 @@ pub enum WorldElement {
     FixedSand,
 }
 
+impl Display for WorldElement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WorldElement::Nothing => write!(f, "."),
+            WorldElement::Wall => write!(f, "#"),
+            WorldElement::NewSand => write!(f, "+"),
+            WorldElement::FixedSand => write!(f, "o"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct World {
     size: Vec2,
     buffer: Vec<WorldElement>,
-    offset: Vec2,
 }
 
 impl World {
-    
-    pub fn new_from_lines(line_defs: &Vec<LineRow>) -> World {
-        let bounding_box: Box = line_defs.iter().collect();
-        let size = bounding_box.size();
 
-        let area = size.product() as usize;
+    pub fn new(size: Vec2) -> World {
+        let Vec2(width, height) = &size;
+        let buffer =vec![WorldElement::Nothing; *width as usize * *height as usize];
 
-        println!("size: {:?}, area: {:?}", size, area);
-
-        let mut world = World {
+        World {
             size,
-            buffer: vec![WorldElement::Nothing; area],
-            offset: bounding_box.top_left().clone(),
-        };
+            buffer,
+        }
+    }
 
+    pub fn insert_lines(&mut self, line_defs: &Vec<LineRow>) {
         for line_def in line_defs {
             let LineRow(lines) = line_def;
             for line in lines {
-                world.insert_wall(line);
+                self.insert_wall(line);
             }
         }
-
-        world
     }
 
     fn insert_wall(&mut self, wall: &Line) {
         let Line(Vec2(sx, sy), Vec2(ex, ey)) = wall;
 
-        for x in *sx..=*ex {
-            for y in *sy..=*ey {
-                self.insert_wall_at(&Vec2(x, y));
+        let asx = min(sx, ex);
+        let aex = max(sx, ex);
+        let asy = min(sy, ey);
+        let aey = max(sy, ey);
+
+        for x in *asx..=*aex {
+            for y in *asy..=*aey {
+                let wall_pos = Vec2(x, y);
+                self.insert_wall_at(&wall_pos);
             }
         }
     }
@@ -193,16 +263,70 @@ impl World {
 
     fn vec2_to_buffer_pos(&self, pos: &Vec2) -> usize {
         let Vec2(x, y) = pos;
-        let Vec2(sx, sy) = &self.size;
-        assert!(x < sx);
-        assert!(y < sy);
+        let Vec2(width, height) = &self.size;
 
-        (y * sx + x) as usize
+        assert!(x < width);
+        assert!(y < height);
+
+        (*y * width + *x) as usize
     }
 
+    pub fn is_in_bounds(&self, pos: &Vec2) -> bool {
+        let Vec2(x, y) = pos;
+        let Vec2(width, height) = &self.size;
 
+        x < width && y < height
+    }
+
+    pub fn view_port(&self) -> ViewPort {
+        ViewPort {
+            world: self,
+            view_port: Box::new_with_dimension(Vec2(494, 0), Vec2(503, 9)),
+        }
+    }
+
+    pub fn get_element_at(&self, pos: &Vec2) -> &WorldElement {
+        let buffer_pos = self.vec2_to_buffer_pos(&pos);
+        &self.buffer[buffer_pos]
+    }
+
+    pub fn try_get_element_at(&self, pos: &Vec2) -> Option<&WorldElement> {
+        if !self.is_in_bounds(&pos) {
+            return None;
+        }
+
+        Some(self.get_element_at(pos))
+    }
+
+    pub fn drop_sand(&mut self, insert_pos: &Vec2) -> bool {
+        todo!()
+    }
 
 }
+
+#[derive(Debug)]
+pub struct ViewPort<'a> {
+    world: &'a World,
+    view_port: Box,
+}
+
+impl<'a> Display for ViewPort<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let Box(Vec2(sx, sy), Vec2(ex, ey)) = &self.view_port;
+
+        for y in *sy..=*ey {
+            for x in *sx..=*ex {
+                let cur = Vec2(x, y);
+                let element = self.world.try_get_element_at(&cur).unwrap_or(&WorldElement::Nothing);
+                write!(f, "{}", element)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -220,8 +344,10 @@ mod test {
         let bounding_box: Box = rows.iter().collect();
         println!("{:?}", bounding_box);
 
-        let world = World::new_from_lines(&rows);
-        println!("{:?}", world);
+        let mut world = World::new(Vec2(504, 10));
+        world.insert_lines(&rows);
+        let view_port = world.view_port();
+        println!("{}", view_port);
     }
 
 }
